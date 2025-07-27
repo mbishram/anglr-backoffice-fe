f8e4c96d80939aed7134409960ff8c5b50166647
Muh Bishram Y A A <m.bishram.yashir@gmail.com>
2025-07-28 07:58:41 +0800

    Lazy load every route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,13 @@
 import { Routes } from '@angular/router';
-import { Auth } from 'app/auth/auth';
-import { Dashboard } from 'app/dashboard/dashboard';
 import { authRoutes } from 'app/auth/auth.route';
 import { authGuard } from 'app/account/auth.guard';
 import { guestGuard } from 'app/account/guest.guard';
-import { Employees } from 'app/employees/employees';
-import { EmployeesDetail } from 'app/employees-detail/employees-detail';
-import { EmployeesCreate } from 'app/employees-create/employees-create';
 
 export const appRoutes: Routes = [
   // Auth routes
   {
     path: 'auth',
-    component: Auth,
+    loadComponent: () => import('app/auth/auth').then((m) => m.Auth),
     canActivate: [guestGuard],
     canActivateChild: [guestGuard],
     children: [
@@ -25,21 +20,33 @@ export const appRoutes: Routes = [
   // Dashboard routes
   {
     path: '',
-    component: Dashboard,
+    loadComponent: () =>
+      import('app/dashboard/dashboard').then((m) => m.Dashboard),
     canActivate: [authGuard],
     canActivateChild: [authGuard],
     children: [
       // Redirect to employees page if user tried to go to '/'
       { path: '', redirectTo: '/employees', pathMatch: 'full' },
-      { path: 'employees', component: Employees, title: 'Employees' },
+      {
+        path: 'employees',
+        loadComponent: () =>
+          import('app/employees/employees').then((m) => m.Employees),
+        title: 'Employees',
+      },
       {
         path: 'employees/create',
-        component: EmployeesCreate,
+        loadComponent: () =>
+          import('app/employees-create/employees-create').then(
+            (m) => m.EmployeesCreate,
+          ),
         title: 'Create Employee',
       },
       {
         path: 'employees/:id',
-        component: EmployeesDetail,
+        loadComponent: () =>
+          import('app/employees-detail/employees-detail').then(
+            (m) => m.EmployeesDetail,
+          ),
         title: 'Detail Employee',
       },
     ],
--- src/app/auth/auth.route.ts
+++ src/app/auth/auth.route.ts
@@ -1,17 +1,16 @@
 import type { AuthRoutes } from 'app/auth/auth.d';
-import { Register } from 'app/register/register';
-import { Login } from 'app/login/login';
 
 export const authRoutes: AuthRoutes = [
   {
     path: 'login',
-    component: Login,
+    loadComponent: () => import('app/login/login').then((m) => m.Login),
     data: { imgSrc: 'assets/images/file-cabinet.jpg', imgAlt: 'File cabinet' },
     title: 'Login',
   },
   {
     path: 'register',
-    component: Register,
+    loadComponent: () =>
+      import('app/register/register').then((m) => m.Register),
     data: {
       imgSrc: 'assets/images/file-cabinet-write.jpg',
       imgAlt: 'File cabinet with person writing',
